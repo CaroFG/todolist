2877e44f693719e909f639f67be9754c3e226f27
CaroFG <carolina.ferreira131@gmail.com>
2019-08-23 12:09:32 +0200

    seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+Card.destroy_all
+Item.destroy_all
+
+5.times do 
+
+	user = User.create!(
+		email: Faker::Internet.email,
+		first_name: Faker::Name.first_name,
+		last_name: Faker::Name.last_name,
+		password: Faker::Internet.password)
+
+	puts "users created"
+
+	card = Card.create!(
+		title: Faker::Lorem.sentence,
+		user: user)
+
+	puts "cards created"
+
+	item = Item.create!(
+		name: Faker::Lorem.sentence)
+
+	puts "items created"
+
+end
