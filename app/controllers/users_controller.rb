16fd6daffdf9d3eccdcf0102b2efd69668da33ed
CaroFG <carolina.ferreira131@gmail.com>
2019-09-04 16:07:11 +0200

    profile and edit user ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,31 @@ class UsersController < ApplicationController
 	
 	def show
 		@user = User.find(params[:id])
-		
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if current_user == @user
+			if @user.update(first_name: params[:first_name], last_name: params[:last_name])
+				redirect_to user_path 
+				flash[:success] = "Votre profil a été édité avec succès"
+			else 
+				flash[:alert] = "Un problème est survenu, veuillez réessayer"
+				render :edit
+			end
+		else
+			redirect_to root_path, notice: "Vous n'avez pas accès à ce profil !"
+		end	
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		redirect_to "/"
+		flash[:success] = "Votre profil a été supprimé avec succès"
 	end
 end
